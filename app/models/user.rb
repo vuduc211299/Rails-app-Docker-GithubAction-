f4a9b8d9080afc9614cdd959ff87acda65b8de32
Vu Duc <vuduc112299@gmail.com>
2024-01-15 15:32:22 +0700

    fix assocation in user model and add foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
-  has_many: images
+  has_many :images
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_10_100047) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_15_075420) do
   create_table "images", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_10_100047) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "users"
 end
